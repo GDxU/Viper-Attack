9437b276ff97b873644851d029b67da80f3f7f87
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-09-23 20:21:57 +0100

    Player Rescue Call and Zooming In

---
+++ Viper-Attack/Assets/Scripts/Eyes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour 
+{
+	private Camera eyes;
+	private float defaultPView;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		eyes = GetComponent<Camera>();
+		defaultPView = eyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButton("Zoom"))
+		{
+			eyes.fieldOfView = defaultPView / 1.5f;
+		}
+		else
+		{
+			eyes.fieldOfView = defaultPView;
+		}
+	}
+}
